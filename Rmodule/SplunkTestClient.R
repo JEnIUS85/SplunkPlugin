507aaca85f9ea5b1c17e0f70bb13183d31605239
JEnIUS85 <nitinkumarsharma.perf@gmail.com>
2017-07-19 18:03:42 +0530

    Updated Splunk Plugin Code

--- Rmodule/SplunkTestClient.R
+++ Rmodule/SplunkTestClient.R
@@ -105,35 +105,3 @@ Initializer <- function(...){
   
   return (propertyFile)
 }# Initializer() Function over..
-
-# +++++=======++++++=====+++++=======TEST CANDIDATES=====+++++=======++++++=====+++++=======
-
-#++++++++++++++++++++++++++ DEFAULT CALL +++++++++++++++++++++++++++++++++++++++++++++++#
-#run <- SplunkInjector()
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
-
-
-#+++++++++++++++++++++++++++SEND LIST AS INPUT ++++++++++++++++++++++++++++++++++++++++++++++#
-#listvariable <- list()
-#listvariable$ApplicationName <- "WCS_Tier"
-#listvariable$ServerName <- "WCS_Server2"
-#listvariable$BusinessTransaction <- "PLP"
-#listvariable$MeasureName <- "CallsPerMinute"
-#listvariable$MeasureValue <- 42
-#run <- SplunkInjector(message=listvariable)
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
-
-#+++++++++++++++++++++++++ SEND XTS OBJECT AS INPUT++++++++++++++++++++++++++++++++++++++++++++++++#
-#inputfile <- "Air_Water_loggers_15min_example.csv"
-#file <- read.csv(inputfile, stringsAsFactors = FALSE)
-
-## FORMAT TIME COLUMN TO POSIXlt
-#file$time15 <- strptime(file$time15, format = "%m/%d/%Y %H:%M")
-
-## ROUND TO NEAREST HOUR
-#file$time15hr <- round(file$time15, "hour")
-
-#df.xts <- xts(x = file[, c(2:7)], order.by = file[, "time15"])
-
-#run <- SplunkInjector(message =df.xts) 
-#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
